a73aacf5090acbacef7c32458e2dcc754472d714
Mukund Bhudia <mukund_bhudia@hotmail.com>
2014-05-17 12:14:21 +0100

    Implemented logout feature

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -53,4 +53,12 @@ public class Application extends Controller {
         );
     }
 
+    public static Result logout() {
+        session().clear();
+        flash("success", "You've been logged out");
+        return redirect(
+                routes.Application.login()
+        );
+    }
+
 }
